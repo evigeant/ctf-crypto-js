804a7108714bd030a42a6806d9cb423c3acf2b21
evigeant <evigeant@okiok.com>
2015-09-06 14:10:05 -0400

    A little more style and catching javascript decoding error

--- ctf-crypto-js.js
+++ ctf-crypto-js.js
@@ -7,12 +7,17 @@ function register(decoderObject) {
 
 //This is the main processing function
 function process(val, output_div) {
-	output_div.innerHTML = val;
+	var out = '<table border="1"><tr><th>Name</th><th>Decoded</th></tr>';
+	out += '<tr><td>Input</td><td>'+val+'</td></tr>';
 	// Process the input in each decoder
 	for (var i = 0; i < decoders.length; i ++) {
-		var decoded = decoders[i].decode(val);
-		output_div.innerHTML += '<br>';
-		output_div.innerHTML += decoded;
+		try {
+			var decoded = decoders[i].decode(val);
+			out += '<tr><td>' + decoders[i].name + '</td><td>'+decoded+'</td></tr>';
+		} catch(err) {
+			out += '<tr><td>' + decoders[i].name + '</td><td>Error: '+err.message+'</td></tr>';
+		}
 	}
-	
+	out += '</table>';
+	output_div.innerHTML = out;
 }
