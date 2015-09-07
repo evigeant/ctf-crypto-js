54a154c1e805a0c6190bc0a6c42346521330dc04
evigeant <evigeant@okiok.com>
2015-09-07 16:57:07 -0400

    Initial framework is in place

--- ctf-crypto-js.js
+++ ctf-crypto-js.js
@@ -52,6 +52,35 @@ function register(decoder) {
 }
 
 /**
+ * Formats the decoded value to be displayed in HTML
+ * 
+ * @param {Decoded}
+ *                decoded The decoded value
+ * @returns {string} The HTML string to insert in the output_div
+ */
+function formatDecoded(decoded) {
+    var out = '';
+    if (decoded.err) {
+	out += '<tr class="error">';
+    } else {
+	out += '<tr>';
+    }
+    out += '<td>' + decoded.name + '</td><td>' + decoded.probability + '</td><td>' + decoded.value + '</td></tr>';
+    return out;
+}
+
+
+/**
+ * A function that compares two Decoded object based on their probability
+ * @param {Decoded} first
+ * @param {Decoded} second
+ * @returns -1 if the first is more probable than second, 0 if both are equal, 1 if second is more probable than first
+ */
+function decodedProbCompare(first, second) {
+    return second.probability - first.probability;
+}
+
+/**
  * This is the main decoding function, it is called from the HTML and can also
  * be called recursively
  * 
@@ -61,17 +90,27 @@ function register(decoder) {
  *                output_div The div in which the output will be rendered
  */
 function process(val, output_div) {
-    var out = '<table border="1"><tr><th>Name</th><th>Decoded</th></tr>';
-    out += '<tr><td>Input</td><td>' + val + '</td></tr>';
+    var decoded_list = [];
     // Process the input in each decoder
     for ( var i = 0; i < decoders.length; i++) {
 	try {
-	    var decoded = decoders[i].decode(val);
-	    out += '<tr><td>' + decoded.name + ' ' + decoded.probability + '</td><td>' + decoded.value + '</td></tr>';
+	    decoded_list.push(decoders[i].decode(val));
 	} catch (err) {
-	    out += '<tr><td>' + decoders[i].name + '</td><td>Error: ' + err.message + '</td></tr>';
+	    var decoded_err = new Decoded(decoders[i].name, 0.0, err.message);
+	    decoded_err.err = true;
+	    decoded_list.push(decoded_err);
 	}
     }
+    
+    decoded_list.sort(decodedProbCompare);
+
+    var out = '<table border="1"><tr><th>Name</th><th>Prob</th><th>Decoded</th></tr>';
+    out += '<tr><td>Input</td><td></td><td>' + val + '</td></tr>';
+    
+    for ( var i = 0; i < decoded_list.length; i++) {
+	out += formatDecoded(decoded_list[i])
+    }
+    
     out += '</table>';
     output_div.innerHTML = out;
 }
